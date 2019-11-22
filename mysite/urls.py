b4bfff604dd838fd377987412d9e3e526acc809a
sarahlevins <sarahjeanlevins@gmail.com>
2019-11-22 12:28:03 +0800

    initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from mysite.core import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+]
